eb50a9b68b52cc50eb3551fefb464e60e0f611fa
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-15 16:45:38 +0300

    Added react-router-dom functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import Weather from "./Weather/Weather";
+import Ninja from "./Ninja Quiz/Ninja";
 
 const App = () => {
   return (
     <div className="min-h-[100dvh] bg-gray-800 text-white">
-      <Weather />
+      <Routes>
+        <Route path='/weather' element={<Weather />}/>
+        <Route path='/ninja' element={<Ninja />}/>
+      </Routes>
     </div>
   )
 }
