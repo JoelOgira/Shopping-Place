b2d58d7a6fa8a31f0128de68c00d79ec85b599fa
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-15 16:46:36 +0300

    Created weather form

--- src/Weather/Form.jsx
+++ src/Weather/Form.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 
 const Form = () => {
+    const [city, setCity] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCity('');
+    }
+
     return (
-        <form onSubmit={(e) => e.preventDefault()}>
-            <input
-                type="text"
-                className="focus:outline-transparent w-full p-2 rounded"
-            />
-        </form>
+        <div className="w-2/3 md:w-1/3">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="focus:outline-transparent w-full p-2 rounded text-black"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+            </form>
+        </div>
     )
 }
 
