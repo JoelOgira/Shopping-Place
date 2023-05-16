aa2bc5c2208e012e5e727a42b96985cb4f22b6ef
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-16 17:45:53 +0300

    Modified the component

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -1,6 +1,8 @@
 import Form from "./Form";
-
+import useAxiosFetch from "../Hooks/useAxiosFetch";
 const Weather = () => {
+    const {data, isLoading, error} = useAxiosFetch();
+
     return (
         <div className="container mx-auto px-2 md:px-0">
             <div className="flex items-center justify-center flex-col py-5 space-y-6">
